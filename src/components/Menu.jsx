3b179b7e4719020382bd3963fb3dfdd56f234985
Anthony Allois <anthonyallois@gmail.com>
2023-11-09 10:26:57 -0300

    feat: Ajustes de pathname

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,18 +1,28 @@
 'use client'
 
+import { navbarLinks } from '@/utils/navbarLinks'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React, { useState } from 'react'
 import { HiMenuAlt3 } from 'react-icons/hi'
 import MenuList from './MenuList'
 
 const Menu = () => {
 
+  const pathname = usePathname();
   const [open, setOpen] = useState(false)
-
   const handleMenu = () => setOpen(!open)
 
   return (
     <>
-      <div className='' onClick={handleMenu} >
+      <nav className='hidden lg:flex items-center gap-5 px-3 text-white font-bold'>
+        {
+          navbarLinks.map(( link ) => (
+            <Link key={link.id} className={`${pathname === link.path ? 'bg-white text-black': ''} hover:text-black hover:bg-white p-2 rounded-md`} href={link.path}>{link.title}</Link>
+          ))
+        } 
+      </nav>
+      <div className='lg:hidden' onClick={handleMenu} >
         <HiMenuAlt3 size={32} className='text-white hover:cursor-pointer' />
       </div>   
 
--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -4,20 +4,24 @@ import Link from 'next/link'
 import React from 'react'
 import { AiOutlineClose } from 'react-icons/ai'
 import { navbarLinks } from '@/utils/navbarLinks'
+import { usePathname } from 'next/navigation'
 
 const MenuList = ({ open, handleMenu }) => {
+
+  const pathname = usePathname();
+
   return (
-    <div onClick={handleMenu} className={`${open ? 'opacity-100 visible' : 'invisible opacity-0'} z-20 transition-all fixed inset-0 bg-black/50 flex justify-end`}>
-      <aside className={`${open ? '' : 'translate-x-48'} transition-all w-48 bg-gray-500`}>
+    <div onClick={handleMenu} className={`${open ? 'opacity-100 visible' : 'invisible opacity-0'} z-20 transition-all fixed inset-0 bg-black/70 flex justify-end`}>
+      <aside className={`${open ? '' : 'translate-x-[50%]'} transition-all w-[50%] bg-black/90`}>
 
         <div className='flex justify-end text-white text-2xl p-5 hover:cursor-pointer' onClick={handleMenu}>
-          <AiOutlineClose size={30} className='text-white hover:text-red-400' />
+          <AiOutlineClose size={30} className='text-white hover:text-black' />
         </div>
 
         <nav className='flex flex-col gap-5 px-3 text-white font-bold'>
           {
             navbarLinks.map(( link ) => (
-              <Link key={link.id} onClick={handleMenu} className='hover:text-black hover:bg-white p-2' href={link.path}>{link.title}</Link>
+              <Link key={link.id} onClick={handleMenu} className={`${pathname === link.path ? 'text-black bg-white' : ''} hover:text-black hover:bg-white p-2 rounded-md`} href={link.path}>{link.title}</Link>
             ))
           }          
         </nav>
