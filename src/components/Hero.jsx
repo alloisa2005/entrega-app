54e466c04a2105a2a36335feb407a0eaa749284f
Anthony Allois <anthonyallois@gmail.com>
2023-11-10 16:13:03 -0300

    feat: Ajustes varios

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className='w-full h-[420px] relative'>
       <Image src={'/images/bg.jpg'} alt='Hero Image' width={1200} height={500} priority className='w-full h-full object-cover' />
       <div className='absolute left-0 top-0 w-full h-full bg-gradient-to-b from-black to-transparent flex items-center px-20'>
-        <h1 className='text-white text-right text-3xl lg:text-5xl italic w-full'>Welcome to <span className='text-5xl font-bold lg:text-6xl text-white'>GamesWorld</span></h1>
+        <h1 className='text-white text-right text-3xl lg:text-5xl italic w-full'>Welcome to <span className='text-5xl font-bold lg:text-6xl bg-clip-text text-transparent pr-2 bg-gradient-to-r from-white to-red-400'>GamesWorld</span></h1>
       </div>      
     </div>
   )
