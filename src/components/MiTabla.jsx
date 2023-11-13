b094a6e0a232b0a30be41e6555d22a7741ad9ee0
Anthony Allois <anthonyallois@gmail.com>
2023-11-13 10:29:16 -0300

    feat: Ajustes en tabla

--- src/components/MiTabla.jsx
+++ src/components/MiTabla.jsx
@@ -1,13 +1,15 @@
 'use client';
 
 import { separadorMiles } from "@/utils/separadorMiles";
-import { useReactTable, getCoreRowModel, flexRender } from "@tanstack/react-table";
+import { useReactTable, getCoreRowModel, flexRender, getPaginationRowModel } from "@tanstack/react-table";
 import Image from "next/image";
+import { useState } from "react";
 import { FaRegEdit } from "react-icons/fa";
 import { MdOutlineDelete }  from "react-icons/md";
 
 const MiTabla = ({ data }) => {
 
+  const [pageIndex, setPageIndex] = useState(1);
   const columns = [
     {
       header: "Foto",
@@ -46,8 +48,8 @@ const MiTabla = ({ data }) => {
     {
       header: "Acciones",
       cell: info => {        
-        return <div className="w-full flex gap-4">
-          <FaRegEdit size={22} className='text-green-400 hover:text-green-500 hover:cursor-pointer hover:scale-110 ease-in duration-300' />
+        return <div className="w-full flex gap-3">          
+          <FaRegEdit size={22} className='text-green-400 hover:text-green-500 hover:cursor-pointer hover:scale-110 ease-in duration-300' />          
           <MdOutlineDelete size={24} className='text-red-400 hover:text-red-500 hover:cursor-pointer hover:scale-110 ease-in duration-300' />           
         </div>
       }
@@ -58,56 +60,58 @@ const MiTabla = ({ data }) => {
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   });
 
 
   return (
-    <table>
-      <thead>
-        {
-          table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id} className="">
-              {
-                headerGroup.headers.map((header) => (
-                  <th key={header.id}>{flexRender(header.column.columnDef.header, header.getContext())}</th>
-                ))
-              }
-            </tr>
-          ))
-        }
-      </thead>
+    <div className="w-full">
+      <table>
+        <thead>
+          {
+            table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id} className="">
+                {
+                  headerGroup.headers.map((header) => (
+                    <th key={header.id}>{flexRender(header.column.columnDef.header, header.getContext())}</th>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </thead>
 
-      <tbody>
-        {
-          table.getRowModel().rows.map( row => (
-            <tr key={row.id}>
-              {
-                row.getVisibleCells().map( (cell, index) => (
-                  <td key={index}>
-                     {flexRender(cell.column.columnDef.cell, cell.getContext()) }
-                  </td>
-                ))
-              }  
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
+        <tbody>
+          {
+            table.getRowModel().rows.map( row => (
+              <tr key={row.id}>
+                {
+                  row.getVisibleCells().map( (cell, index) => (
+                    <td key={index}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext()) }
+                    </td>
+                  ))
+                }  
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+
+      <div className="mt-2 flex items-center justify-center gap-4">
+          <button onClick={() => {
+            setPageIndex(pageIndex-1)
+            table.setPageIndex(0)
+            }}>{'<<'}</button>          
+          <p>{`Página ${pageIndex} de ${table.getPageCount()}`}</p>
+          <button onClick={() => {
+            setPageIndex(pageIndex+1)
+            table.setPageIndex(table.getPageCount()-1)
+            }}>{'>>'}</button>          
+      </div>
+    </div>
   )
 }
 
 export default MiTabla
 
-/*
-{data.map((item) => (
-          <tr key={item.id} className=''>
-            <td className="w-[70px]">
-              <Image src={item.image} alt={item.name} width={50} height={50} className="w-[55px] object-contain" />
-            </td>
-            <td>{item.name}</td>            
-            <td>{item.price}</td>
-            <td>{item.stock}</td>
-          </tr>
-        ))}
-
-*/
