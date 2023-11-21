b076fa5f259c41fea54104b8a070d96b29106924
Anthony Allois <anthonyallois@gmail.com>
2023-11-21 13:59:02 -0300

    Ajustes varios

--- src/components/MensajeContacto.jsx
+++ src/components/MensajeContacto.jsx
@@ -5,7 +5,7 @@ const MensajeContacto = () => {
   return (
     <div className='font-montserrat contenedor text-black py-8'>
       <h2 className='font-semibold italic text-md md:text-xl lg:text-2xl uppercase border-b-2 border-black'>
-        ¿Buscas un juego que no está en la tienda?
+        Comentarios o Sugerencias
       </h2>      
 
       <MensajeForm />
