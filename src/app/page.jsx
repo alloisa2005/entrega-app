7bcec33fd20cb09d52473de67a2198e6a9b6079c
Anthony Allois <anthonyallois@gmail.com>
2023-12-02 16:55:56 -0300

    Agregando Suspense

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import FilaJuegos from "@/components/FilaJuegos";
 import Hero from "@/components/Hero";
 import Leyenda from "@/components/Leyenda";
 import MensajeContacto from "@/components/MensajeContacto";
+import { Suspense } from "react";
 
 
 export default function Home() {  
@@ -12,8 +13,14 @@ export default function Home() {
       <Hero />
 
       <Leyenda />
+
+      <Suspense fallback={<div className="bg-black h-40 flex items-center justify-center text-white">
+        <p className="font-montserrat font-bold text-2xl italic">Loading...</p>
+      </div>}>
+        <FilaJuegos />            
+      </Suspense>
+
       
-      <FilaJuegos />            
 
       <Editoriales />
 
