34855457e62e39bdc707462b9df14d7f6b9aabb2
Anthony Allois <anthonyallois@gmail.com>
2023-12-11 21:43:27 -0300

    Ajustes

--- src/app/admin/mensajes/page.jsx
+++ src/app/admin/mensajes/page.jsx
@@ -4,17 +4,17 @@ import React from 'react'
 
 const Mensajes = async () => {
 
-  //const mensajes = await getAllMensajes(); 
+  const mensajes = await getAllMensajes(); 
 
   return (
     <div className='my-4 mb-10 font-montserrat'>      
-      {/* {
-        mensajes.length === 0 ? (
+       {
+        mensajes && mensajes.length === 0 ? (
           <div className='text-center my-16'>
             <h2 className='font-bold text-[22px] mb-6'>No tiene mensajes.</h2>            
           </div>
         ) : <TablaMensajes data={mensajes} /> 
-      }       */}
+      }
     </div>
   )
 }
--- src/app/admin/productos/page.jsx
+++ src/app/admin/productos/page.jsx
@@ -1,6 +1,5 @@
 
 import React from 'react'
-import { mockGames } from '../../../data/products'
 import TablaProductos from '@/components/TablaProductos';
 import { getProductos } from '@/utils/juegos/juegos';
 import Link from 'next/link';
@@ -12,7 +11,7 @@ const ListaProductos = async () => {
   return (
     <div className='my-4 mb-10 font-montserrat'>      
       {
-        juegos.length === 0 ? (
+        juegos && juegos.length === 0 ? (
           <div className='text-center my-16'>
             <h2 className='font-bold text-2xl mb-6'>No tiene juegos dados de alta.</h2>
             <Link href={'/admin/nuevoJuego'} className='bg-black text-white px-4 py-3 font-bold rounded-md' >Agregar Juego</Link>
